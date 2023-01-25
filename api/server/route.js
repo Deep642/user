653dbd60e141fc39628817a422f713ec78068800
Deep642 <89216609+Deep642@users.noreply.github.com>
2023-01-25 16:52:43 +0530

    Update route.js

--- api/server/route.js
+++ api/server/route.js
@@ -1,4 +1,4 @@
-import express from 'express';
+const express=require("express");
 import { getUsers, addUser, getUserById, editUser, deleteUser } from '../controller/user-controller.js';
 
 const router = express.Router();
